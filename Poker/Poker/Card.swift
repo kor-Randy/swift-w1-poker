c3015d1723e357d0f232605a8f224bf7c17b5baf
Tree.JI <tree.ji@kakaocorp.com>
2021-01-07 12:03:29 +0900

    Make TestCode Deck , Change Card, Deck from class to struct

--- Poker/Poker/Card.swift
+++ Poker/Poker/Card.swift
@@ -7,36 +7,39 @@
 
 import Foundation
 
-class Card{
+struct Card{
     //enum Type case 별 Loop를 위해 CaseIterable 채택
     enum Shape: String, CaseIterable{
         case spade = "♠"
         case club = "♣"
         case heart = "♥"
         case diamond = "♦"
+        case joker = "🃏"
     }
     
     //can't change card's shape and num
     private(set) var shape: Shape
-    private(set) var num: Int
+    private(set) var num: Int // Joker has not Num
     
     var description: String{
-        "\(self.shape.rawValue)\(self.convertNumToString(num: self.num))"
+        "\(self.shape.rawValue)\(self.convertNumToString())"
     }
     
-    init(shape: Shape, num: Int){
+    init(shape: Shape, num: Int = 0){
         self.shape = shape
         self.num = num
     }
     
-    private func convertNumToString(num: Int) -> String{
-        if self.num == 1{
+    private func convertNumToString() -> String{
+        if num == 0 {
+            return "Joker"
+        } else if num == 1{
             return "A"
-        } else if self.num == 11{
+        } else if num == 11{
             return "J"
-        } else if self.num == 12{
+        } else if num == 12{
             return "Q"
-        } else if self.num == 13{
+        } else if num == 13{
             return "K"
         } else{
             return String(num)
--- Poker/Poker/Deck.swift
+++ Poker/Poker/Deck.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-class Deck{
+//상속이 필요없기에 struct로 구현
+struct Deck{
     private var cardDeck: [Card] = []
     
     var count: Int{
@@ -15,28 +16,49 @@ class Deck{
     }
     
     init(){
-        initCardDeck()
+        _ = initCardDeck()
     }
     
-    func initCardDeck(){
+    mutating func initCardDeck() -> Self{
+        cardDeck.removeAll()
+        
+        print("카드 전체를 초기화 했습니다.")
+        
         for shape in Card.Shape.allCases{
+            if shape == .joker{
+                continue
+            }
             for num in 1...13{
                 cardDeck.append(Card(shape: shape, num: num))
             }
         }
+        
+        cardDeck.append(Card(shape: .joker, num: 0))
+        
+        print("총 \(self.count)장의 카드가 있습니다.")
+        return self
     }
     
-    func shuffle(){
-        cardDeck.shuffle()
+    mutating func shuffleDeck() -> Self{
+        print("총 \(self.count)장의 카드를 섞었습니다.")
+        
+        self.cardDeck.shuffle()
+        return self
     }
     
-    func removeOne() -> Card?{
-        count ? cardDeck.removeFirst() : nil
+    mutating func removeOne() -> Card?{
+        let cardOnTop = cardDeck.removeFirst()
+        
+        print(cardOnTop.description)
+        print("총 \(self.count)장의 카드가 남아있습니다.")
+        
+        return count > 0 ? cardOnTop : nil
     }
     
-    func reset(){
+    mutating func resetDeck() -> Self{
         cardDeck.removeAll()
-        initCardDeck()
+        _ = initCardDeck()
+        return self
     }
     
 }
